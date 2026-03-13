cba6ddb963da82480461c9911de4cb14a3f6bd2d
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-13 00:21:54 -0300

    Refactor main function to simplify input loop

--- 2.c
+++ 2.c
@@ -5,36 +5,17 @@
 
 #include<stdio.h>
 
-void main()
-
-{
-
+void main(){
 	int num, soma=0, cont;
+	for(cont=1; cont<=5; cont++){
+	   printf("Digite um algarismos:");
+       scanf(" %d", &num);
 
-	
-
-	for(cont=1; cont<=5; cont++)
-
-	{
-
-
-
-	printf("Digite um algarismos:");
-
-	scanf(" %d", &num);
-
-	if (num%2==0)
-
-		{
-
-		soma+=num;
-
-		}
-
+	   if (num%2==0){
+	    	soma+=num;
+	   }
 	}
-
 	printf("soma=%d", soma);
-
 }
 
 
