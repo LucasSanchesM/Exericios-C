e6c39a3b2cded040587c446abc1e857e09c727f1
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-13 00:18:43 -0300

    Add initial comments for C program requirements

--- 1.c
+++ 1.c
@@ -1,3 +1,9 @@
+//Desenvolva um programa em C que receba um número inteiro inicial.
+//O programa deve imprimir uma sequência numérica crescente a partir desse 
+//número, respeitando as seguintes regras:
+//Filtro: Não devem ser impressos os números que sejam múltiplos de 3.
+//Parada: A sequência deve ser interrompida imediatamente após o programa 
+//encontrar o primeiro número que seja múltiplo de 10 (este último número deve ser testado, mas só será impresso se não for múltiplo de 3)."
 #include<stdio.h>
 
 
