3700bbb7b32c873a02fd2034781f6bdc50061036
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-13 00:34:41 -0300

    Add program description comment in 5.c
    Add a comment block describing the program's functionality.

--- 5.c
+++ 5.c
@@ -1,3 +1,11 @@
+/*
+Construa um programa em C que solicite ao usuário um valor inteiro n.
+O programa deve exibir na tela os primeiros n números ímpares positivos,
+começando a partir do 1. Utilize uma estrutura de repetição que controle a
+quantidade de números exibidos,
+independentemente de quantas iterações sejam necessárias para encontrá-los.
+*/
+
 #include<stdio.h>
 
 void main(){
