ff27b3ee38a125d342230617463b2c62b6894c58
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-03-13 00:29:19 -0300

    Fix formatting and improve code readability

--- 4.c
+++ 4.c
@@ -3,31 +3,16 @@
 //ao final, exibir a soma total de todos esses números.
 
 #include<stdio.h>
-
 void main()
-
 {
-
 	int n,cont, soma=0;
-
 	printf("Digite o n:");
-
 	scanf("%d", &n);
 
-	
-
 	for(cont=0; cont<=n; cont++)
-
 	{
-
 		printf("%d ", cont);
-
 		soma+=cont;
-
 	}
-
 	printf("\n%d", soma);
-
 }
-
-
